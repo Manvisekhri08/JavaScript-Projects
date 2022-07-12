ff5acb01ea0434804c63f9be2c4bbc56019b3ab7
Manvisekhri08 <manvisekhri08@gmail.com>
2022-07-13 00:55:00 +0530

    Understanding eventlisteners and working with classes

---
+++ 03 - Modal Window/script.js
@@ -0,0 +1,27 @@
+const modal = document.querySelector('.modal');
+
+const overlay = document.querySelector('.overlay');
+
+const hidden = document.querySelector('.hidden');
+
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for(let i = 0; i < btnShowModal.length; i++) {
+    btnShowModal[i].addEventListener('click', openModal);
+}
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
